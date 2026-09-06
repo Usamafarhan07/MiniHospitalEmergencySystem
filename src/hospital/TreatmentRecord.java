5a35246cfe1ae4ca03d55ed15a4b55e230d7f0cf
usamafarhan07 <usamafarhan003@gmail.com>
2026-09-06 16:33:01 +0530

    Create treatment record model

---
+++ src/hospital/TreatmentRecord.java
@@ -0,0 +1,10 @@
+package hospital;
+
+public class TreatmentRecord {
+
+    private int patientId;
+    private String patientName;
+    private String doctorName;
+    private String treatment;
+    private String date;
+}
