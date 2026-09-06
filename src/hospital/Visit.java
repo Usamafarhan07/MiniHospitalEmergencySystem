13be609d3a0b35bcfb813fd4eb0dc0efe36faad1
usamafarhan07 <usamafarhan003@gmail.com>
2026-09-06 16:38:50 +0530

    package hospital;

---
+++ src/hospital/Visit.java
@@ -0,0 +1,10 @@
+package hospital;
+
+public class Visit {
+
+    private int visitId;
+    private String visitDate;
+    private String doctorName;
+    private String diagnosis;
+    private String treatment;
+}
