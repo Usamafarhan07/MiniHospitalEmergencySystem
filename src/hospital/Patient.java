9b52d6b238d8997b5c21ddb6881a56b7789fee17
usamafarhan07 <usamafarhan003@gmail.com>
2026-09-06 16:21:48 +0530

    Create Patient model class

---
+++ src/hospital/Patient.java
@@ -0,0 +1,10 @@
+package hospital;
+
+public class Patient {
+
+    private int patientId;
+    private String patientName;
+    private int age;
+    private String contactNumber;
+    private String medicalCondition;
+}
